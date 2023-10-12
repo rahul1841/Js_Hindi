943709b9da1d5e0ca37550a3659e79909efae43d
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2023-10-12 13:46:54 +0000

    container config done

---
+++ container/01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Heloo") // we are using nodeJs, not browser
+
+console.log(3+3) // code readiability should be high
+console.log("Rahul")
+
+// datatypes
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// primitive datatypes
+
+// number => 2 to power 23
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+
+console.log(typeof "hitesh");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
