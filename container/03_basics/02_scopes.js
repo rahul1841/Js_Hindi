380a97f2688125de23b16e4603e1094e503c5ca0
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2023-10-18 10:47:09 +0000

    done

--- container/03_basics/02_scopes.js
+++ container/03_basics/02_scopes.js
@@ -3,12 +3,12 @@ let a = 300
 
 
 
-if(true){
-let a  = 10
-const b = 20
-// var c = 30
-console.log("INNER: ", a);
-}
+// if(true){
+// let a  = 10
+// const b = 20
+// // var c = 30
+// console.log("INNER: ", a);
+// }
 
 
 // for (let i = 0; i < array.length; i++) {
@@ -17,8 +17,55 @@ console.log("INNER: ", a);
 // }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "hitesh"
+    if(username == "hitesh"){
+        const website = " youtube"
+      //  console.log(username + website);
+    }
+   // console.log(website);
+}
+
+// console.log(username);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+
+// console.log(addtwo(5));    // gives error
+
+const addtwo = function(num){
+    return num + 2
+}
+
+
+
 
 
