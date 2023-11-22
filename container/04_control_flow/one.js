4025dea02d5f244c5844c1e6fe51eeda4e7b9f0c
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2023-11-22 03:10:47 +0000

    done

---
+++ container/04_control_flow/one.js
@@ -0,0 +1,52 @@
+//if 
+
+// const isUserLoggedIn = true
+// const temp = 41
+// if(temp){
+// console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");
+// }
+// <, >, <=, >=, ==, !=, ===(check datatypetype also), !==
+
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+const userloggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if(userloggedin && debitcard){
+    console.log("Allow to buy course");
+}
+
+if(loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in");
+}
