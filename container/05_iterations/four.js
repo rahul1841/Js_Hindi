502e6a4cd5b25e27103f14f131a62c40e8445541
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2023-11-23 18:20:29 +0000

    done

--- container/05_iterations/four.js
+++ container/05_iterations/four.js
@@ -6,6 +6,22 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(key);
+   // console.log(`${key} shortcut is for ${myObject[key]}` );
 
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+// console.log(key);
+// }
+
