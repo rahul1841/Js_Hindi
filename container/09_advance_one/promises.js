5c0d06f3ce3e27b291910a79540bd04b6c8b8e8c
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2023-12-03 08:21:00 +0000

    done

---
+++ container/09_advance_one/promises.js
@@ -0,0 +1,13 @@
+// fetch('https://something.com').then().catch().finally()
+
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("promise consumed");
+})
